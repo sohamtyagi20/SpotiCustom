0ab23055ecb49761c10641f2a3f8db20bf02dadc
sohamtyagi20 <132931450+sohamtyagi20@users.noreply.github.com>
2025-12-24 17:17:04 -0500

    Removed 127 link and added variables

--- apiCall.js
+++ apiCall.js
@@ -8,7 +8,7 @@
 // - forMe.html:   getTrackRec(), getArtistRec()
 // - success.html: getTopSongsGlobal()
 
-const SERVER_URL = "http://127.0.0.1:8000";
+const SERVER_URL = window.location.origin;
 const TOKEN_COOKIE = "accessToken";
 
 // ---------------------------
